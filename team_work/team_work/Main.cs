1016785440dedab752f7d7c862097ac7e350e4db
Irina <iirraallii@gmail.com>
2015-04-14 09:24:21 +0300

    another slashes

--- team_work/team_work/Main.cs
+++ team_work/team_work/Main.cs
@@ -63,6 +63,7 @@ namespace team_work
         }
         //
         //показать...
+        //
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
         {
             string message =
