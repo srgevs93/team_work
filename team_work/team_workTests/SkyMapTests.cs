e0ece49421804b9656c096eb923d6ed99f1e4da5
Ilya Vassyutovich <MONOmah_V@mail.ru>
2015-05-06 20:22:32 +0300

    Проверка работы юнит-тестов в CI
    TODO
    Удалить старый проект для тестов;

---
+++ team_work/team_workTests/SkyMapTests.cs
@@ -0,0 +1,22 @@
+﻿using team_work;
+using NUnit.Framework;
+using System.Xml;
+
+namespace team_workTests
+{
+    [TestFixture]
+    public class SkyMapTests
+    {
+        [Test, ]
+        public void Foo()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        [Test]
+        public void Bar()
+        {
+            Assert.That(false, Is.True);
+        }
+    }
+}
